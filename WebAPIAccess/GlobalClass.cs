34ca72de7f3153ca1fe88d97255395eafc40753f
vjade24 <vincenjade.vj@gmail.com>
2023-06-15 10:30:04 +0800

    Commit CAFOA List

---
+++ WebAPIAccess/GlobalClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPIAccess
+{
+    public class GlobalClass
+    {
+        public const string connetionString_pay = @"Data Source=HRIS-DO-BACKUP\HRIS_DEV;Initial Catalog=HRIS_PAY;User ID=sa;Password=SystemAdmin_DEV123";
+        public const string connetionString_ats = @"Data Source=HRIS-DO-BACKUP\HRIS_DEV;Initial Catalog=HRIS_ATS;User ID=sa;Password=SystemAdmin_DEV123";
+    }
+}
---
+++ WebAPIAccess/Models/sp_api_cafoa.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPIAccess.Models
+{
+    public class sp_api_cafoa
+    {
+        public int payroll_year                { get; set; }
+        public string payroll_month            { get; set; }
+        public string payroll_registry_nbr     { get; set; }
+        public string payrolltemplate_code     { get; set; }
+        public string payrolltemplate_descr    { get; set; }
+        public string payroll_registry_descr   { get; set; }
+        public DateTime payroll_period_from    { get; set; }
+        public DateTime payroll_period_to      { get; set; }
+        public string function_code            { get; set; }
+        public string doc_cafoa                { get; set; }
+        public string doc_voucher_nbr          { get; set; }
+        public string employment_type          { get; set; }
+        public string post_status_descr        { get; set; }
+        public decimal gross_pay               { get; set; }
+        public decimal net_pay                 { get; set; }
+    }
+}
