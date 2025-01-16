b23e8f231838a22fa176d3448e4e9da2172c7a3f
marvinolita09231980 <marvinolitagithub@gmail.com>
2025-01-16 10:40:47 +0800

    commit

---
+++ WebAPIAccess/Models/EmpoloyeeInformationModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPIAccess.Models
+{
+    public class EmpoloyeeInformationModel
+    {
+        public string empl_id { get; set; }
+        public string employee_name { get; set; }
+        public string last_name { get; set; }
+        public string first_name { get; set; }
+        public string middle_name { get; set; }
+        public string suffix_name { get; set; }
+        public string postfix_name { get; set; }
+        public string courtisy_title { get; set; }
+        public string gender { get; set; }
+        public string birth_date { get; set; }
+        public string age { get; set; }
+      
+
+    }
+}
+
