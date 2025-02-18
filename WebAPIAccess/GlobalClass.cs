d2573869f4a664b23f24e194e3bd0b367ab8d9a0
marvinolita09231980 <marvinolitagithub@gmail.com>
2025-02-18 08:31:23 +0800

    commit

--- WebAPIAccess/GlobalClass.cs
+++ WebAPIAccess/GlobalClass.cs
@@ -7,7 +7,7 @@ namespace WebAPIAccess
 {
     public class GlobalClass
     {
-        public const string connetionString_pay = @"Data Source=HRISD\HRIS;Initial Catalog=HRIS_PAY;User ID=sa;Password=SystemAdmin_PRD123";
-        public const string connetionString_ats = @"Data Source=HRISD\HRIS;Initial Catalog=HRIS_ATS;User ID=sa;Password=SystemAdmin_PRD123";
+        public const string connetionString_pay = @"Data Source=192.168.80.49\HRIS;Initial Catalog=HRIS_PAY;User ID=sa;Password=SystemAdmin_PRD123";
+        public const string connetionString_ats = @"Data Source=192.168.80.49\HRIS;Initial Catalog=HRIS_ATS;User ID=sa;Password=SystemAdmin_PRD123";
     }
 }
